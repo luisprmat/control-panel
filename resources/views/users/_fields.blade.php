8e05047b84f3fe7e074ab27c2799977a01156e9e
Luis Antonio Parrado <luisprmat@gmail.com>
2020-09-10 18:10:57 -0500

    Fix tests and change dusk database name

--- resources/views/users/_fields.blade.php
+++ resources/views/users/_fields.blade.php
@@ -70,7 +70,7 @@
             type="checkbox"
             id="skill_{{ $skill->id }}"
             value="{{ $skill->id }}"
-            {{ $errors->any() ? old("skills.{$skill->id}") : $user->skills->contains($skill) ? 'checked' : '' }}>
+            {{ $errors->any() ? old("skills.{$skill->id}") : ( $user->skills->contains($skill) ? 'checked' : '' )}}>
         <label class="custom-control-label" for="skill_{{ $skill->id }}">{{ $skill->name }}</label>
     </div>
 @endforeach
